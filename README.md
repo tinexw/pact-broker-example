7ab57602492861fce691073c962ab4450e5a3498
tinexw <kristine.jetzke@kreuzwerker.de>
2018-07-01 13:19:00 +0200

    Upgrade pact broker CLI

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 This repo contains code to reproduce a potential bug in the pact broker / can-i-deploy command.
-Run ./pact-broker-steps.sh to create the scenario in the https://test.pact.dius.com.au pact broker. You need to replace all occurrences of pact-broker-username and pact-broker-password with the actual pact broker credentials.
+Run `./pact-broker-steps.sh` to create the scenario in the https://test.pact.dius.com.au pact broker. 
+Before running it you need to
+- update the path to the pact broker CLI, search for `export PACT_BROKER_CLI_HOME=`
+- replace all occurrences (in all files) of pact-broker-username and pact-broker-password with the actual pact broker credentials.  
--- pact-broker-steps.sh
+++ pact-broker-steps.sh
@@ -3,6 +3,7 @@
 export PACT_BROKER_BASE_URL=https://test.pact.dius.com.au
 export PACT_BROKER_USERNAME=pact-broker-username
 export PACT_BROKER_PASSWORD=pact-broker-password
+export PACT_BROKER_CLI_HOME=~/pact-broker-1.47.1
 
 curl -XDELETE $PACT_BROKER_BASE_URL/pacticipants/de.tine.foo -v -u $PACT_BROKER_USERNAME:$PACT_BROKER_PASSWORD
 curl -XDELETE $PACT_BROKER_BASE_URL/pacticipants/de.tine.bar -v -u $PACT_BROKER_USERNAME:$PACT_BROKER_PASSWORD
@@ -15,15 +16,15 @@ cd consumer
 cd ../provider-bar
 ../mvnw clean verify -Dpact.provider.version=10.0.0 -Dpact.verifier.publishResults=true
 
-~/pact-broker-1.47.0/bin/pact-broker can-i-deploy -a de.tine.foo -e 1.0.0 --to prod
-~/pact-broker-1.47.0/bin/pact-broker create-version-tag -a de.tine.bar -e 10.0.0 -t prod
-~/pact-broker-1.47.0/bin/pact-broker can-i-deploy -a de.tine.foo -e 1.0.0 --to prod
+$PACT_BROKER_CLI_HOME/bin/pact-broker can-i-deploy -a de.tine.foo -e 1.0.0 --to prod
+$PACT_BROKER_CLI_HOME/bin/pact-broker create-version-tag -a de.tine.bar -e 10.0.0 -t prod
+$PACT_BROKER_CLI_HOME/bin/pact-broker can-i-deploy -a de.tine.foo -e 1.0.0 --to prod
 
 cd ../provider-baz
 ../mvnw clean verify -Dpact.provider.version=20.0.0 -Dpact.verifier.publishResults=true
-~/pact-broker-1.47.0/bin/pact-broker can-i-deploy -a de.tine.foo -e 1.0.0 --to prod
-~/pact-broker-1.47.0/bin/pact-broker create-version-tag -a de.tine.baz -e 20.0.0 -t prod
-~/pact-broker-1.47.0/bin/pact-broker can-i-deploy -a de.tine.foo -e 1.0.0 --to prod
+$PACT_BROKER_CLI_HOME/bin/pact-broker can-i-deploy -a de.tine.foo -e 1.0.0 --to prod
+$PACT_BROKER_CLI_HOME/bin/pact-broker create-version-tag -a de.tine.baz -e 20.0.0 -t prod
+$PACT_BROKER_CLI_HOME/bin/pact-broker can-i-deploy -a de.tine.foo -e 1.0.0 --to prod
 
 cd ../consumer
 ../mvnw clean verify -Dtest=de.tine.consumer.BazContractTest,de.tine.consumer.Bar2ContractTest
@@ -32,9 +33,9 @@ cd ../consumer
 cd ../provider-bar
 ../mvnw clean verify -Dpact.provider.version=11.0.0 -Dpact.verifier.publishResults=true
 
-~/pact-broker-1.47.0/bin/pact-broker can-i-deploy -a de.tine.foo -e 2.0.0 --to prod
-~/pact-broker-1.47.0/bin/pact-broker create-version-tag -a de.tine.bar -e 11.0.0 -t prod
-~/pact-broker-1.47.0/bin/pact-broker can-i-deploy -a de.tine.foo -e 2.0.0 --to prod
+$PACT_BROKER_CLI_HOME/bin/pact-broker can-i-deploy -a de.tine.foo -e 2.0.0 --to prod
+$PACT_BROKER_CLI_HOME/bin/pact-broker create-version-tag -a de.tine.bar -e 11.0.0 -t prod
+$PACT_BROKER_CLI_HOME/bin/pact-broker can-i-deploy -a de.tine.foo -e 2.0.0 --to prod
 
 cd ../consumer
 ../mvnw clean verify -Dtest=de.tine.consumer.BazContractTest,de.tine.consumer.Bar3ContractTest
@@ -43,5 +44,5 @@ cd ../consumer
 cd ../provider-bar
 ../mvnw clean verify -Dpact.provider.version=10.0.0 -Dpact.verifier.publishResults=true
 
-~/pact-broker-1.47.0/bin/pact-broker can-i-deploy -a de.tine.foo -e 3.0.0 --to prod
+$PACT_BROKER_CLI_HOME/bin/pact-broker can-i-deploy -a de.tine.foo -e 3.0.0 --to prod
 
